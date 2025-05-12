d5751a0a0b702392cb49868fbdf3b73ab1cd50df
Ricky Kaare Engelharth <ricky@ricky.dk>
2025-05-12 21:44:41 +0200

    Revert "Make MessageId required in Metadata"
    This reverts commit 223611346589e9e261458fa3e9b6e6b2775ce168.

--- src/Cabazure.Messaging.Abstractions/IsExternalInit.cs
+++ src/Cabazure.Messaging.Abstractions/IsExternalInit.cs
@@ -3,12 +3,3 @@ namespace System.Runtime.CompilerServices;
 #pragma warning restore IDE0130 // Namespace does not match folder structure
 
 internal static class IsExternalInit { }
-
-#pragma warning disable CA1018 // Mark attributes with AttributeUsageAttribute
-internal sealed class RequiredMemberAttribute : Attribute { }
-
-internal sealed class CompilerFeatureRequiredAttribute : Attribute
-{
-    public CompilerFeatureRequiredAttribute(string name) { }
-}
-#pragma warning restore CA1018 // Mark attributes with AttributeUsageAttribute
