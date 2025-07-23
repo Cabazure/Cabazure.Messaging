eecbb5ec98c91c8c921885c3c9dcf9a5af1e480a
Christian Helle <christian.helle@outlook.com>
2025-07-23 14:00:13 +0200

    Add XML documentation to IEventHubPublisherFactory.Create method

--- src/Cabazure.Messaging.EventHub/IEventHubPublisherFactory.cs
+++ src/Cabazure.Messaging.EventHub/IEventHubPublisherFactory.cs
@@ -5,6 +5,12 @@
 /// </summary>
 public interface IEventHubPublisherFactory
 {
+    /// <summary>
+    /// Creates an Event Hub publisher for the specified message type.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of message that the publisher will handle.</typeparam>
+    /// <param name="connectionName">The optional name of the connection configuration to use. If not specified, the default connection will be used.</param>
+    /// <returns>An Event Hub publisher instance for the specified message type.</returns>
     IEventHubPublisher<TMessage> Create<TMessage>(
         string? connectionName = null);
 }
