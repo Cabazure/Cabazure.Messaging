956711203f18270c17d93b94b4cb085cac47172f
Christian Helle <christian.helle@outlook.com>
2025-07-23 14:10:08 +0200

    Add XML documentation to IStorageQueuePublisherFactory.Create method

--- src/Cabazure.Messaging.StorageQueue/IStorageQueuePublisherFactory.cs
+++ src/Cabazure.Messaging.StorageQueue/IStorageQueuePublisherFactory.cs
@@ -5,6 +5,12 @@
 /// </summary>
 public interface IStorageQueuePublisherFactory
 {
+    /// <summary>
+    /// Creates a Storage Queue publisher for the specified message type.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of message that the publisher will handle.</typeparam>
+    /// <param name="connectionName">The optional name of the connection configuration to use. If not specified, the default connection will be used.</param>
+    /// <returns>A Storage Queue publisher instance for the specified message type.</returns>
     IStorageQueuePublisher<TMessage> Create<TMessage>(
         string? connectionName = null);
 }
