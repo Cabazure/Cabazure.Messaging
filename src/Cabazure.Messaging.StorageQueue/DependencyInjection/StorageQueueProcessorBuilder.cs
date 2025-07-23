bff12c32b56d628c0c2e7a6bbc3ad4c4395e03c9
Christian Helle <christian.helle@outlook.com>
2025-07-23 14:13:27 +0200

    Add XML documentation to StorageQueueProcessorBuilder properties and methods

--- src/Cabazure.Messaging.StorageQueue/DependencyInjection/StorageQueueProcessorBuilder.cs
+++ src/Cabazure.Messaging.StorageQueue/DependencyInjection/StorageQueueProcessorBuilder.cs
@@ -7,8 +7,16 @@ namespace Cabazure.Messaging.StorageQueue.DependencyInjection;
 /// </summary>
 public class StorageQueueProcessorBuilder
 {
+    /// <summary>
+    /// Gets or sets the processor options that control the behavior of the Storage Queue processor.
+    /// </summary>
     public StorageQueueProcessorOptions Options { get; set; } = new();
 
+    /// <summary>
+    /// Configures the polling interval for checking the queue for new messages.
+    /// </summary>
+    /// <param name="pollingInterval">The time interval between polling operations.</param>
+    /// <returns>The current builder instance for method chaining.</returns>
     public StorageQueueProcessorBuilder WithPollingInterval(
         TimeSpan pollingInterval)
     {
@@ -16,6 +24,11 @@ public class StorageQueueProcessorBuilder
         return this;
     }
 
+    /// <summary>
+    /// Configures whether the queue should be created if it does not exist.
+    /// </summary>
+    /// <param name="createIfNotExists">True to create the queue if it doesn't exist; otherwise, false.</param>
+    /// <returns>The current builder instance for method chaining.</returns>
     public StorageQueueProcessorBuilder WithInitialization(
         bool createIfNotExists)
     {
