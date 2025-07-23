3a6aac21821f9038f23d017f523b54aab96aba2b
Christian Helle <christian.helle@outlook.com>
2025-07-23 14:10:53 +0200

    Add XML documentation to StorageQueuePublishingOptions properties

--- src/Cabazure.Messaging.StorageQueue/StorageQueuePublishingOptions.cs
+++ src/Cabazure.Messaging.StorageQueue/StorageQueuePublishingOptions.cs
@@ -5,7 +5,13 @@
 /// </summary>
 public class StorageQueuePublishingOptions : PublishingOptions
 {
+    /// <summary>
+    /// Gets or sets the visibility timeout for the message, specifying how long the message will be invisible after being dequeued. If not specified, the default visibility timeout configured on the queue will be used.
+    /// </summary>
     public TimeSpan? VisibilityTimeout { get; set; }
     
+    /// <summary>
+    /// Gets or sets the time-to-live duration for the message. If not specified, the message will not expire.
+    /// </summary>
     public TimeSpan? TimeToLive { get; set; }
 }
