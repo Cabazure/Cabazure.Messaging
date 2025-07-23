704c9c59289a1b13bec1e2de51ed20b173fda12d
Christian Helle <christian.helle@outlook.com>
2025-07-23 14:03:59 +0200

    Add XML documentation to ServiceBusPublishingOptions properties

--- src/Cabazure.Messaging.ServiceBus/ServiceBusPublishingOptions.cs
+++ src/Cabazure.Messaging.ServiceBus/ServiceBusPublishingOptions.cs
@@ -5,9 +5,18 @@
 /// </summary>
 public class ServiceBusPublishingOptions : PublishingOptions
 {
+    /// <summary>
+    /// Gets or sets the time-to-live duration for the message. If not specified, the default TTL configured on the queue or topic will be used.
+    /// </summary>
     public TimeSpan? TimeToLive { get; set; }
 
+    /// <summary>
+    /// Gets or sets the session identifier for the message when using session-aware queues or topics.
+    /// </summary>
     public string? SessionId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the scheduled time when the message should be made available for processing. If not specified, the message will be available immediately.
+    /// </summary>
     public DateTimeOffset? ScheduledEnqueueTime { get; set; }
 }
